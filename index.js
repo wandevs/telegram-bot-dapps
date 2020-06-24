36133c7a3637db4c66051aaa79cd441a1da62a84
Mo Lin <molin@wanchain.org>
2020-06-24 15:43:02 +0800

    update jackpot

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ const robotSchedules = () => {
 robotSchedules();
 
 let messageModel = `
-Hello, everyone! 
-It's my honor to introduce the latest status of Jack's Pot DApp on Wanchain.
-
----- The No-loss lottery Jack's Pot ----
-Jack's Pot's total stake pool has reached $TOTAL_POOL$ Wan.
-The jackpot amount for this round is $PRIZE_POOL$ Wan.
-( Welcome join us to play in Wan Wallet DApps or in website https://jackspot.finnexus.app/ )
-----------------------------------------`
+---- Jack's Pot Insight ----
+Jackpot:    $PRIZE_POOL$ WAN
+Pool:       $TOTAL_POOL$ WAN
+Players:    $TOTAL_PLAYER$
+Tickets:    $TOTAL_TICKETS$
+Win Number: $WIN_NUMBER$
+Winners:    $WINNERS$
+( Welcome to play Jack's Pot in Wan Wallet DApps or in website https://jackspot.finnexus.app/ )`
 
 let messageModel2 = `
 Hello, everyone! 
@@ -40,7 +40,7 @@ Wandora Box had $WANDORA_AMOUNT$ wan trade in last 24 hours.
 
 const jacksPotSC = "0x76b074d91f546914c6765ef81cbdc6f9c7da5685";
 
-async function getInfos() {
+async function getJacksPotInfos() {
     let msg = messageModel;
 
     while (true) {
@@ -51,12 +51,76 @@ async function getInfos() {
     }
 
     let web3 = getWeb3();
-    let jackspot = new web3.eth.Contract(jackspotAbi, jacksPotSC);
-    let poolInfo = await jackspot.methods.poolInfo().call();
+    let sc = new web3.eth.Contract(jackspotAbi, jacksPotSC);
+
+
+    let funcs = [];
+    funcs.push(sc.methods.poolInfo().call());
+    funcs.push(sc.getPastEvents('Buy', { fromBlock: 8865321 }));
+    funcs.push(sc.getPastEvents('LotteryResult', { fromBlock: 8865321 }));
+
+    const [poolInfo, buyEvents, settleEvents] = await Promise.all(funcs);
+
     let totalPool = (Number(web3.utils.fromWei(poolInfo.delegatePool)) + Number(web3.utils.fromWei(poolInfo.demandDepositPool)) + Number(web3.utils.fromWei(poolInfo.prizePool))).toFixed(1);
     let pricePool = Number(web3.utils.fromWei(poolInfo.prizePool)).toFixed(1);
     msg.replace("$TOTAL_POOL$", totalPool);
     msg.replace("$PRIZE_POOL$", pricePool);
+
+    let winCode = 0;
+    let winCount = 0;
+    winCount = Number(settleEvents[i].returnValues.amounts[0]) > 0 ? settleEvents[settleEvents.length - 1].returnValues.amounts.length : 0;
+    winCode = settleEvents[settleEvents.length - 1].returnValues.winnerCode;
+    
+    msg.replace('$WIN_NUMBER$', winCode);
+    msg.replace('$WINNERS$', winCount);
+
+
+    let playerData = [];
+    funcs = [];
+    for (let i=0; i<buyEvents.length; i++) {
+      funcs.push(sc.methods.getUserCodeList(buyEvents[i].returnValues.user).call());
+    }
+
+    let users = await Promise.all(funcs);
+
+    let addresses = [];
+    let tickets = [];
+    let tmpTickets = [];
+    for (let i=0; i<buyEvents.length; i++) {
+      let totalStakeAmount = 0;
+      for (let m=0; m<users[i].amounts.length; m++) {
+        totalStakeAmount += Number(web3.utils.fromWei(users[i].amounts[m]));
+      }
+
+      let one = {
+        address: buyEvents[i].returnValues.user.toLowerCase(),
+        ticketsCount: users[i].codes.length,
+        totalStakeAmount,
+        key: i
+      };
+
+      if (Number(one.ticketsCount) > 0 && Number(one.totalStakeAmount) > 0 && !addresses.includes(one.address)) {
+        playerData.push(one);
+        addresses.push(one.address);
+        for (let m=0; m<users[i].codes.length; m++) {
+          if (!tmpTickets.includes(users[i].codes[m])) {
+            tmpTickets.push(users[i].codes[m]);
+            tickets.push({
+              ticket:Number(users[i].codes[m]),
+              count:1,
+              stake: Number(web3.utils.fromWei(users[i].amounts[m]))
+            });
+          } else {
+            let id = tmpTickets.indexOf(users[i].codes[m]);
+            tickets[id].count++;
+            tickets[id].stake += Number(web3.utils.fromWei(users[i].amounts[m]));
+          }
+        }
+      }
+    }
+
+    msg.replace("$TOTAL_TICKETS$", tickets.length.toString());
+    msg.replace("$TOTAL_PLAYER$", playerData.length.toString());
 
     return msg;
 }
